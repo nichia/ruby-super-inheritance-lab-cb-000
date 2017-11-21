5a8ce4f30d9e55630abb42af5019324a1afbc918
Ni Chia <nichia@gmail.com>
2017-11-21 15:27:50 +0000

    automatically backed up by learn

---
+++ lib/student.rb
@@ -0,0 +1,11 @@
+module Student
+  module InstanceMethods
+    def hello
+      puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand
+      puts "Pick me!"
+    end
+  end
+end
